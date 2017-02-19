a323e33ee154f97f6909b4308bb414aeb7085fd2
Rich Cherngchaosil <richer_po@hotmail.com>
2017-02-18 21:54:43 -0800

    Admin Login

--- js/AngularJS/controller_00_main.js
+++ js/AngularJS/controller_00_main.js
@@ -3,4 +3,37 @@ beachLiveApp.controller('main_controller', function($scope, data){
 	$scope.data = data;
 
 
+
+/** Admin Login **/
+
+	$scope.adminAccess = false; // I know..., client still need authentication to firebase in order to access database.
+
+	// Need to connect to firebase and authenticate for admin user only
+	// for now, fake login
+	$scope.failed = false;
+
+	var admin_auth = {
+		user: "admin",
+		password: "1234"
+	}
+
+	$scope.admin = {
+		user: "userName",
+		password: "password"
+	}
+
+	$scope.login = function(){
+		// console.log($scope.admin.user);
+		// console.log($scope.admin.password);
+		if($scope.admin.user == admin_auth.user && $scope.admin.password == admin_auth.password){
+			console.log("Logged In");
+			$scope.adminAccess = true;
+			$scope.failed = false;
+			$('#loginModal').modal('toggle') 
+		} else {
+			$scope.failed = true;
+			// $scope.$apply();
+			console.log("Failed Login");
+		}
+	}
 });
