6334d3e6ce654e47abf9c74932eb76dfcdc39c9b
Rich Cherngchaosil <richer_po@hotmail.com>
2017-02-20 18:40:25 -0800

    Admin Announcement Page

--- js/AngularJS/controller_01_announcement.js
+++ js/AngularJS/controller_01_announcement.js
@@ -1,3 +1,16 @@
 beachLiveApp.controller('announcement_controller', function($scope, data){
 	$scope.content = data.anouncement_content;
+
+
+
+
+	/** Announcement Admin **/
+	$scope.deleteAnnouncement = function(_index){
+		console.log(_index);
+	}
+
+	$scope.announce = function(){
+		console.log($scope.message)
+		$scope.message = "";
+	}
 });
