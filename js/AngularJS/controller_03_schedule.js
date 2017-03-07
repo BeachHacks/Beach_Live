f6babe62e017b9d9f122d5c75885357ba27fff54
Rich Cherngchaosil <richer_po@hotmail.com>
2017-03-07 09:48:05 -0800

    Forming Schedule

--- js/AngularJS/controller_03_schedule.js
+++ js/AngularJS/controller_03_schedule.js
@@ -1,3 +1,13 @@
 beachLiveApp.controller('schedule_controller', function($scope, data, AngFirebase, $state){
 	$scope.test = "hello world";
+	var TIME_START 	= "2017.4.22.9";
+	var TIME_END 	= "2017.4.23.12";
+
+
+	console.log(arrayOfHour(TIME_START, TIME_END));
 });
+
+
+function arrayOfHour(_start, _end){
+	var arr = []
+}
