7f772d65e1a3cd65ffc812fd3792e04d5397fbc3
Rich Cherngchaosil <richer_po@hotmail.com>
2017-03-07 17:26:36 -0800

    Change login Structure to promise

---
+++ js/AngularJS/controller_login.js
@@ -0,0 +1,37 @@
+beachLiveApp.controller('login_controller', function($scope, AngFirebase, $state){
+
+	/** Admin Login **/
+
+	$scope.failed = false;
+
+	$scope.login = function(_userName, _password){
+		// Login to Firebase
+		AngFirebase.login(_userName, _password, function(user){
+			if(user){
+                // User is signed in.
+                $scope.failed = false;
+                $('#loginModal').modal('hide')
+				console.log("Logged In")
+				
+				// BUG...
+				setTimeout(function(){
+				    //do what you need here
+					$state.go("admin.announcement")
+				}, 1000);
+                
+            } else {
+                // No user is signed in.
+	            $scope.failed = true;
+	         	console.log("Failed Login");
+
+            }
+            // Clear  
+			$scope.admin.user = "";
+			$scope.admin.password = "";
+
+			if(!$scope.$$phase) {
+				$scope.$apply();
+			}
+		}); 
+	}
+});
