0dab9259f3c9e0d94b3f8d9d78def580dbca3580
Rich Cherngchaosil <richer_po@hotmail.com>
2017-03-21 16:37:11 -0700

    Fixed sidebar hidden, generate schedule hour

--- js/AngularJS/controller_03_schedule.js
+++ js/AngularJS/controller_03_schedule.js
@@ -1,7 +1,10 @@
 beachLiveApp.controller('schedule_controller', function($scope, data, AngFirebase, $state){
 
 	var month = ['JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN','JUL', 'AUG', 'SEPT', 'OCT', 'NOV', 'DEC']; // Need to be refactor and move the service
-	var day = ['SUN'];
+	var day = ['SUN', 'MON', 'TUE', 'WED', 'THURS', 'FRI', 'SAT'];
+
+	var start_time = "22-04-2017 10:00";
+	var end_time = "23-04-2017 15:00";
 
 	var getDate = function(_strDate){
 		dateTimeParts = _strDate.split(' ');
@@ -11,15 +14,64 @@ beachLiveApp.controller('schedule_controller', function($scope, data, AngFirebas
 		return new Date(dateParts[2], parseInt(dateParts[1], 10) - 1, dateParts[0], timeParts[0], timeParts[1]);
 	}
 
-	var start_time = getDate("22-04-2017 10:00");
-	var end_time = getDate("23-04-2017 15:00");
+	var formatTime = function(_hour){
+		var formatHour = "";
 
-	console.log(start_time.getTime());
-	console.log(start_time);
-	console.log(start_time.getDate());
-	console.log(start_time.getDay());
-	console.log(start_time.getMonth())
-	console.log(start_time.getHours());
-	console.log(start_time.getMinutes())
+		var dd = "am";
+		var h = _hour;
+		if (h >= 12) {
+			h = _hour - 12;
+			dd = "pm";
+		}
+		if (h == 0) {
+			h = 12;
+		}
+
+		h = h<10?"0"+h:h;
+		return h + dd;
+	}
+
+	var createScheduleHour = function(_start, _end){
+		var schedule = [];
+
+		var date_start = getDate(_start);
+		var date_end 	= getDate(_end);
+
+		for(var i = date_start.getTime(); i < date_end.getTime(); i += 3600000){
+			var newDate = new Date(i);
+			var d = {};
+			d['month'] 	= month[newDate.getMonth()];
+			d['date'] 	= newDate.getDate();
+			d['day'] 	= day[newDate.getDay()];
+			d['hour']	= formatTime(newDate.getHours());
+
+			// console.log(newDate.getDate() + " " + newDate.getDay() + " " + newDate.getHours() + " " + newDate.getMinutes());
+			schedule.push(d);
+		}
+
+		return schedule;
+	}
+
+
+	$scope.schedule = createScheduleHour(start_time, end_time);
+	// var date_start = getDate(start_time);
+	// var date_end = getDate(end_time);
+
+	// for(var i = date_start.getTime(); i < date_end.getTime(); i += 3600000){
+	// 	var newDate = new Date(i);
+
+	// 	console.log(newDate.getDate() + " " + newDate.getDay() + " " + newDate.getHours() + " " + newDate.getMinutes());
+	// }
+
+	// console.log(date_start.getTime());
+	// console.log(date_end.getTime());
+
+	// console.log(date_start.getTime());
+	// console.log(date_start);
+	// console.log(date_start.getDate());
+	// console.log(date_start.getDay());
+	// console.log(date_start.getMonth())
+	// console.log(date_start.getHours());
+	// console.log(date_start.getMinutes())
 
 });
