3f4a6a83c8b97d5636fd1edc76b98d276b4d5999
Rich Cherngchaosil <richer_po@hotmail.com>
2017-04-04 22:16:17 -0700

    public mentor Page

--- js/AngularJS/controller_02_mentor.js
+++ js/AngularJS/controller_02_mentor.js
@@ -1,3 +1,14 @@
 beachLiveApp.controller('mentor_controller', function($scope, data, AngFirebase, $state){
-	$scope.test = "hello world";
+
+	$scope.selected_radio = {
+		option : "default"
+	}
+	$scope.radio_option = ["Web", "iOS", "Android", "VR", "Hardware", "Others"];
+
+	$scope.submit = function(){
+		console.log("submit");
+		console.log($scope.tableNum);
+		console.log($scope.selected_radio.option);
+		console.log($scope.description);
+	}
 });
