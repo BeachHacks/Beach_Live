1f1d3f7a14ba42d08294322564af0b09c70738b5
jaylenw <mr2cleanj@live.com>
2017-04-19 23:31:18 -0700

    in progress for slack integration

--- js/AngularJS/controller_02_mentor.js
+++ js/AngularJS/controller_02_mentor.js
@@ -1,4 +1,4 @@
-beachLiveApp.controller('mentor_controller', function($scope, data, AngFirebase, $state){
+beachLiveApp.controller('mentor_controller', ['$scope', '$http', function($scope, $http, data, AngFirebase, $state){
 
 	$scope.requestList = AngFirebase.getRequestList();
 
@@ -22,7 +22,30 @@ beachLiveApp.controller('mentor_controller', function($scope, data, AngFirebase,
 		}
 
 		AngFirebase.writeRequestMentor(request);
-	}
+
+		var payload = {"text": "Name: " + $scope.name + "\n" + $scope.tableNum + "\n"
+		 		+ $scope.selected_radio.option + "\n" + $scope.description
+			}
+
+
+		// POST Request to our mentor Slack channel
+		$http({
+		  method: 'POST',
+		  url: 'https://hooks.slack.com/services/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',
+			headers: {
+   			'Content-Type': 'application/x-www-form-urlencoded'
+ 			},
+ 			data: payload
+		}).then(function successCallback(response) {
+		    // this callback will be called asynchronously
+		    // when the response is available
+				console.log("sent to slack");
+		  }, function errorCallback(response) {
+		    // called asynchronously if an error occurs
+		    // or server returns response with an error status.
+				console.log("failed to send to slack");
+		  });
+		}
 
 
 	// Trigger rerender
@@ -33,4 +56,4 @@ beachLiveApp.controller('mentor_controller', function($scope, data, AngFirebase,
 			$scope.$apply();
 		}
 	});
-});
+}]);
