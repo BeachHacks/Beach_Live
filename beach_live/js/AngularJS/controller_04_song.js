00b464789a3614a8ad67c9562538e1ee4dfa6289
jaylenw <mr2cleanj@live.com>
2017-04-21 02:35:34 -0700

    fixed controller bugs and made page better

--- beach_live/js/AngularJS/controller_04_song.js
+++ beach_live/js/AngularJS/controller_04_song.js
@@ -2,8 +2,8 @@ beachLiveApp.controller('song_controller', function($scope, $http){
 
   $scope.submitSong = function(){
     //initializing message from attendee
-    var message = ":musical_note" + "n\*Song Title:* " + $scope.title
-    + "n\*Artist:* " + $scope.artist;
+    var message = ":musical_note:" + "\n*Song Title:* " + $scope.title
+    + "\n*Artist(s):* " + $scope.artist;
 
     //initializing payload
     var payload = {
@@ -23,14 +23,14 @@ beachLiveApp.controller('song_controller', function($scope, $http){
         // when the response is available
         console.log("sent to slack");
         alert("Thank you. Your song request has been sent to our DJ.")
+        $scope.title = "";
+        $scope.artist = "";
       }, function errorCallback(response) {
         // called asynchronously if an error occurs
         // or server returns response with an error status.
         console.log("failed to send to slack");
         alert("Uh oh :(. Looks like we had trouble sending your request to our DJ.")
       });
-      $scope.song = "";
-      $scope.artist = "";
   }
 
 });
